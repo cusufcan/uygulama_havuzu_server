b430697040982dd0d5f3686dd28286b900b354e1
Yusuf Can Mercan <cusufcan@gmail.com>
2024-08-16 16:00:33 +0300

    finished infrastructure layer

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using uygulama_havuzu_server.Core.Entities;
+
+namespace uygulama_havuzu_server.Infrastructure.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using uygulama_havuzu_server.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Added Connection String & DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
