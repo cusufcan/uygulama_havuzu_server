42f665336dc35678a6df3c86efa47593838ad2c5
Yusuf Can Mercan <cusufcan@gmail.com>
2024-08-16 15:43:43 +0300

    finished core folder

---
+++ Core/Entities/User.cs
@@ -0,0 +1,7 @@
+﻿namespace uygulama_havuzu_server.Core.Entities {
+    public class User {
+        public required int Id { get; set; }
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+    }
+}
