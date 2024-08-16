bc04a72e44e1113d4ee2a102ed8fac4aa40c06a7
Yusuf Can Mercan <cusufcan@gmail.com>
2024-08-16 15:53:05 +0300

    application layer finished

--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace uygulama_havuzu_server.Core.Entities {
     public class User {
-        public required int Id { get; set; }
+        public int Id { get; set; }
         public required string Username { get; set; }
         public required string PasswordHash { get; set; }
     }
