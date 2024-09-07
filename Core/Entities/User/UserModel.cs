677cbfaacc8b9283a1825c51a7c1564903a49e5c
Yusuf Can Mercan <cusufcan@gmail.com>
2024-09-07 08:57:29 +0300

    fix

--- Core/Entities/User/UserModel.cs
+++ Core/Entities/User/UserModel.cs
@@ -1,7 +1,5 @@
-﻿namespace uygulama_havuzu_server.Core.Entities.User
-{
-    public class UserModel
-    {
+﻿namespace uygulama_havuzu_server.Core.Entities.User {
+    public class UserModel {
         public int Id { get; set; }
         public required string Username { get; set; }
         public required string Password { get; set; }
